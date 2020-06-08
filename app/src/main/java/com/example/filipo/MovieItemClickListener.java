e7f4094ae56cb7c6891dc55341466114b91c9054
fatemehmohammadi79 <58948784+fatemehmohammadi79@users.noreply.github.com>
2020-06-08 13:16:08 +0430

    Completing UI

--- app/src/main/java/com/example/filipo/MovieItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.filipo;
-
-import android.widget.ImageView;
-
-public interface MovieItemClickListener {
-    //we will need the imageView to make the shared animation between the tow activity
-    void onMoveClick(Movie movie, ImageView movieImageView);
-}
